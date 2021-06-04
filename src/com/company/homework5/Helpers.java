2d28566278d90a0324fac574a2a7a25377043084
karakonstantin <persivall619@gmail.com>
2021-06-04 22:34:47 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/com/company/homework5/Helpers.java
@@ -0,0 +1,14 @@
+package com.company.homework5;
+
+public class Helpers {
+    static class HelperDistanceStatic {
+        public void HelperDistanceStaticCalculate(int X1, int X2, int Y1, int Y2) {
+        }
+    }
+
+    static class HelperMathematics {
+        public void calculate(int x0, int y0) {
+
+        }
+    }
+}
---
+++ src/com/company/homework5/HelpersTest.java
@@ -0,0 +1,13 @@
+package com.company.homework5;
+
+public class HelpersTest {
+    public static void main(String[] args) {
+        Helpers.HelperMathematics helpers = new Helpers.HelperMathematics();
+        helpers.calculate(100, 6093);
+
+        Helpers.HelperDistanceStatic helpers1 = new Helpers.HelperDistanceStatic();
+
+        helpers1.HelperDistanceStaticCalculate(3452, 31234134, 31244, 34545);
+    }
+
+}
