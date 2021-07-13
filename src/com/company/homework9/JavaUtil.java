d3bb8bcf404600abf06023696722057c1bd72468
karakonstantin <persivall619@gmail.com>
2021-07-13 15:18:02 +0300

    Merge remote-tracking branch 'origin/master'

--- src/com/company/homework9/JavaUtil.java
+++ src/com/company/homework9/JavaUtil.java
@@ -1,6 +1,6 @@
 package com.company.homework9;
 
-public class Util {
+public class JavaUtil {
     public static double getDistance(int x1, int y1, int x2, int y2){
         double number = Math.sqrt((y1-x1)*(y1-x1)+(y2-x2)*(y2-x2));
         return number;
---
+++ src/com/company/homework9/JavaUtilTest.java
@@ -0,0 +1,7 @@
+package com.company.homework9;
+
+public class JavaUtilTest {
+    public static void main(String[] args) {
+        System.out.println(JavaUtil.getDistance(5,6,5,10));
+    }
+}
